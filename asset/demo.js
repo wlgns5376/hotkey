dde54441cb2ce98bafa871d9f56f571d9998a14c
wlgns5376 <wlgns5376@naver.com>
2014-12-31 16:26:29 +0900

    Update demo.js

--- asset/demo.js
+++ asset/demo.js
@@ -2,7 +2,11 @@ $(function(){
 
     Hotkey(document.body, 'keydown', {
         'ctrl+z' : function( event ){
-            console.log(event.data);
+            $('#hotkey-' + event.data.keys.replace('+', '-')).addClass('active');
         }
+    });
+    
+    $(document.body).on('keyup', function(){
+       $('.hotkeys').removeClass('active');
     });
 });
